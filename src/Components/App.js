ef9cfeb6dcd31daa711e80b5e73622b13510f29b
Halık Ali TUNÇ <tunchalikali@gmail.com>
2021-03-09 21:16:17 +0300

    3 days weather added

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,6 +7,7 @@ const App = () => {
   const [location, setLocation] = useState('Istanbul');
   const [weatherInfo, setWeatherInfo] = useState({})
   const [city, setcity] = useState('')
+  const [weatherInforForSevenDays, setweatherInforForSevenDays] = useState([])
 
   useEffect(() => {
       fetch(`http://api.weatherapi.com/v1/current.json?key=af1f9a8f56364e889a4100341210703&q=${location}`)
@@ -18,7 +19,12 @@ const App = () => {
     setcity(event.target.value)
     city.length >3 && setLocation(city)
   }
-
+  
+  const onClickHandler = (event) => {
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=af1f9a8f56364e889a4100341210703&q=${location}&days=7`)
+    .then(response => response.json())
+    .then(data => setweatherInforForSevenDays(data.forecast.forecastday));
+  }
 
   return (
     <div className="weather-wrapper">
@@ -44,9 +50,6 @@ const App = () => {
               <div className="mountain4"></div>
               <div className="bottom">
                 <div className="cloud">
-                  {/* <div className="cloud1"></div>
-                  <div className="cloud2"></div>
-                  <div className="cloud3"></div> */}
                 <img src={weatherInfo.data.current.condition.icon}  className="cloud1 cloud2 cloud3"/> 
                 </div>
               
@@ -54,6 +57,41 @@ const App = () => {
               </div>
               </div>
             </>  
+          }
+
+          <button className="button" onClick={(event) => onClickHandler (event)}>3 Günlük </button>
+          {weatherInfo.data && 
+            <div className="seven-days">
+              {weatherInforForSevenDays.map(item => (
+             <div className="three-days-wrapper">
+                  <div className="container" id="container">
+                <span className="city">{item.date}  </span>
+                <span className="temp">{item.day.avgtemp_c} &#176;C</span>
+                <div className="wcloud">
+                  <div className="cloud1" style={{backgroundColor: 'white', boxShadow: 'none'}}></div>
+                  <div className="cloud2" style={{backgroundColor: 'white', boxShadow: 'none'}}></div>
+                  <div className="cloud3" style={{backgroundColor: 'white', boxShadow: 'none'}}></div>
+                </div>
+                <div className="wcloud2">
+                  <div className="cloud1" style={{backgroundColor: 'white', boxShadow: 'none'}}></div>
+                  <div className="cloud2" style={{backgroundColor: 'white', boxShadow: 'none'}}></div>
+                  <div className="cloud3" style={{backgroundColor: 'white', boxShadow: 'none'}}></div>
+                </div>
+                <div className="mountain1"></div>
+                <div className="mountain2"></div>
+                <div className="mountain3"></div>
+                <div className="mountain4"></div>
+                <div className="bottom">
+                  <div className="cloud">
+                  <img src={item.day.condition.icon}  className="cloud1 cloud2 cloud3"/> 
+                  </div>
+                
+                  <p className="summary">{item.day.condition.text}</p>
+                </div>
+                </div>
+             </div>
+              ))}
+            </div>
           }
     </div>
   );
